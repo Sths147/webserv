c1b7ff891496d854eec5977d819dfe6a713368a6
Sths147 <simon.thomas.tux@gmail.com>
2025-08-27 14:16:45 +0200

    response ok

--- inc/Response.hpp
+++ inc/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: sithomas <sithomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 14:45:52 by sithomas          #+#    #+#             */
-/*   Updated: 2025/08/27 11:27:28 by sithomas         ###   ########.fr       */
+/*   Updated: 2025/08/27 13:49:17 by sithomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ class Response
 
 		const std::string	determine_final_path(Request& request, Server& server);
 		void				set_error_response();
-		void				set_status(unsigned short int& code);
+		void				set_status(const unsigned short int& code);
 		void				write_response(int&	client_fd);
 		void				set_get_response();
 		void				set_get_headers();
